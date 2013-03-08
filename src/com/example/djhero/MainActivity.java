954862ed4be3f25db69f16119f417149588464ed
Nikola Radoicic <n.radoicic@gmail.com>
2013-03-08 00:08:51 -0800

    Removed bin and added sample to MainActivity

--- src/com/example/djhero/MainActivity.java
+++ src/com/example/djhero/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.djhero;
 
-import android.os.Bundle;
+import java.util.ListIterator;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
-
 import android.view.View;
-import android.widget.EditText; 
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -14,8 +14,6 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		songList songlist = new songList("Shake it:Moe:20|Why can't you love me:Gursimran:40");
-		songlist.getListNames();
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
@@ -27,27 +25,29 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	public void onRefreshClick(View view) {
-		
-		
 		 StringBuilder strb = new StringBuilder("");
-         strb.append( "Whats up my people?\n");
-         strb.append( "We taking over!");
+		 
+		 // Create new songlist element
+         songList songlist = new songList("Shake it:Moe:20|Why can't you love me:Gursimran:40|I ate a shit ton of Reese's cups:Sukhi Man:30");
          
+         // Iterate though the list and display the information
+         ListIterator<Song> it = songlist.Songs.listIterator();
+ 		 while(it.hasNext()){
+ 			Song nextsong = it.next();
+ 			strb.append("Title:  " + nextsong.Title + "\n");
+ 			strb.append("Artist:  " + nextsong.Artist + "\n");
+ 			strb.append("Length:  " + nextsong.Length + "\n");
+ 			strb.append("\n");
+ 		 }
+ 		 
          // Rather than writing to a pre-allocated field on the
          // screen, create a new view, and write to it.
-         
          RelativeLayout top_linear_layout =
-                    (RelativeLayout) findViewById(R.id.RelativeLayoutMain);
+        		 (RelativeLayout) findViewById(R.id.RelativeLayoutMain);
          
          TextView report_line = new TextView(this);
          report_line.setText(strb);
          top_linear_layout.addView(report_line);
 
 	}
-	
-	public void getListOfSongs(){
-		
-	}
-	
-
 }
