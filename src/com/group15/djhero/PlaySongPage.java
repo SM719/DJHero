9fb9dad6d6ef34192f62aa41cf5716ac2c4f93ef
Sukhi <sm_719@hotmail.com>
2013-04-01 16:31:30 -0700

    added change artist for when song finishes on its one

--- src/com/group15/djhero/PlaySongPage.java
+++ src/com/group15/djhero/PlaySongPage.java
@@ -3,11 +3,13 @@ package com.group15.djhero;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -28,6 +30,8 @@ public class PlaySongPage extends Activity implements OnSeekBarChangeListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_play_song_page);
+		final ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
 		
 		 myApp = (MyApplication) PlaySongPage.this.getApplication();
 		 myApp.imageView = (ImageView) findViewById(R.id.imageView1);
@@ -89,6 +93,16 @@ public class PlaySongPage extends Activity implements OnSeekBarChangeListener {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.play_song_page, menu);
 		return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch(item.getItemId()){
+		case android.R.id.home:
+			super.onBackPressed();
+			return true;
+		default:
+		return super.onOptionsItemSelected(item);
+		}
 	}
 
 	public void PausePlay(View view) {
@@ -220,6 +234,7 @@ public class PlaySongPage extends Activity implements OnSeekBarChangeListener {
 						
 						myApp.textViewforSongPosition.setText(myApp.songlist.Songs.get(myApp.positionOfSong).Title);
 						myApp.imageView.setImageBitmap(myApp.images[myApp.positionOfSong]);
+						myApp.textViewforSongArtist.setText(myApp.songlist.Songs.get(myApp.positionOfSong).artist);
 					}
 				});
 				
